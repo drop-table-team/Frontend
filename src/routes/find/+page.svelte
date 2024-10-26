f16defc8bd7982ecb8e9879700026dd1369ce1f9
Max Gerbeth <78021255+xamtodd@users.noreply.github.com>
2024-10-26 19:06:14 +0200

    Tagfrontend (#7)
    * tags suche
    
    * del dev

--- src/routes/find/+page.svelte
+++ src/routes/find/+page.svelte
@@ -1,5 +1,22 @@
 <script>
-
+    import { Tabs, TabItem } from 'flowbite-svelte';
+    import TagSearch from '$lib/components/+TagSearch.svelte';
 </script>
- 
-<h1>Find</h1>
+  
+<div class="p-4 max-w-xl mx-auto">
+  <Tabs tabStyle="full" defaultClass="flex rounded-lg divide-x rtl:divide-x-reverse divide-gray-200 shadow dark:divide-gray-700">
+    <TabItem open class="w-full">
+        <span slot="title">Tags-Suche</span>
+        <TagSearch />
+    </TabItem>
+    <TabItem class="w-full">
+      <span slot="title">Dashboard</span>
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Dashboard:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+  </Tabs>
+</div>
+
+  
