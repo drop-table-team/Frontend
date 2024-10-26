daa594ef3bda13072166ca97af869c30bac3e440
János Euler <janos.euler@gmail.com>
2024-10-26 16:50:35 +0200

    moved files (#4)

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,5 +1,25 @@
 <script>
-
+    import Filemock from '$lib/components/+Filemock.svelte';
+    import Textmock from '$lib/components/+Textmock.svelte';
+    import { Alert } from 'flowbite-svelte';
+    import { Tabs, TabItem } from 'flowbite-svelte';
+    let uploadModules = [
+        {name: 'Einfacher Upload', component: Filemock},
+        {name: 'Dokumente', component: Filemock},
+        {name: 'Bilder', component: Filemock},
+        {name: 'Audio', component: Filemock},
+        {name: 'Text', component: Textmock},
+    ];
 </script>
- 
-<h1>Upload</h1>
+  
+<div class="p-8">
+          <Tabs>
+            {#each uploadModules as module}
+              <TabItem open title={module.name}>
+                <!--<p class="text-sm text-gray-500 dark:text-gray-400">-->
+                        <module.component />
+                <!--</p>-->
+              </TabItem>
+            {/each}
+          </Tabs>
+</div>
