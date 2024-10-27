ab7395a11a8e811867ea35fb195c34c059a2a123
Max Gerbeth <maxgerbeth03@gmail.com>
2024-10-27 03:55:06 +0100

    fix darkmode bug

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,45 +1,50 @@
-<div class="container mx-auto p-4">
-    <h1 class="font-bold mb-4">Impressum</h1>
+<script lang="ts">
 
-    <h2 class="font-semibold mt-4">Anschrift</h2>
-    <p>
+    import { P, Heading } from 'flowbite-svelte';
+</script>
+
+<div class="container p-4 max-w-8xl mx-auto">
+    <Heading tag="h3" class="font-bold mb-4">Impressum</Heading>
+
+    <Heading tag="h5" class="font-semibold mt-4">Anschrift</Heading>
+    <P>
         Hochschule Fulda <br />
-        Leipzieger Straße 123 <br />
+        LeiPzieger Straße 123 <br />
         36039 Fulda <br />
-    </p>
+    </P>
 
-    <h2 class="font-semibold mt-6">Vertreten durch</h2>
-    <p>'; DROP TABLE TEAM; --</p>
+    <Heading tag="h5" class="font-semibold mt-6">Vertreten durch</Heading>
+    <P>'; DROP TABLE TEAM; --</P>
 
-    <h2 class="font-semibold mt-6">Kontakt</h2>
-    <p>
+    <Heading tag="h5" class="font-semibold mt-6">Kontakt</Heading>
+    <P>
         Telefon: +49 123 456789 <br />
         E-Mail:
-        <a href="mailto:info@ddt.de" class="text-blue-600">info@ddt.de</a>
-    </p>
+        <a href="mailto:info@ddt.de" class="text-blue-600">info@dtt.de</a>
+    </P>
 
-    <h2 class="font-semibold mt-6">Haftungsausschluss</h2>
-    <p>
+    <Heading tag="h5" class="font-semibold mt-6">Haftungsausschluss</Heading>
+    <P>
         Die bereitgestellten Informationen in dieser Wissensdatenbank sind ausschließlich für interne Zwecke innerhalb des Organisationsnetzwerks gedacht.
         Eine Weitergabe oder der Zugriff von außerhalb der Organisation ist nicht gestattet.
         Wir übernehmen keine Haftung für die Richtigkeit, Vollständigkeit oder Aktualität der Informationen. Jegliche Nutzung erfolgt auf eigene Gefahr.
-    </p>
+    </P>
 
-    <h2 class="font-semibold mt-6">Datenschutz</h2>
-    <p>
+    <Heading tag="h5" class="font-semibold mt-6">Datenschutz</Heading>
+    <P>
         Die Daten und Informationen, die in dieser Wissensdatenbank gespeichert sind, bleiben innerhalb des Organisationsnetzwerks. 
-        Es werden keine personenbezogenen Daten ohne Zustimmung der Betroffenen erhoben oder verarbeitet. 
+        Es werden keine Personenbezogenen Daten ohne Zustimmung der Betroffenen erhoben oder verarbeitet. 
         Weitere Informationen zum Datenschutz entnehmen Sie bitte unserer.
-    </p>
+    </P>
 
-    <h2 class="font-semibold mt-6">Urheberrecht</h2>
-    <p>
+    <Heading tag="h5"  class="font-semibold mt-6">Urheberrecht</Heading>
+    <P>
         Die Inhalte dieser Wissensdatenbank sind urheberrechtlich geschützt. 
         Eine Vervielfältigung oder Verwendung in anderen elektronischen oder gedruckten Publikationen ist ohne ausdrückliche Zustimmung der [Organisation] nicht gestattet.
-    </p>
+    </P>
 
-    <h2 class="font-semibold mt-6">Schlussbestimmungen</h2>
-    <p>
+    <Heading tag="h5"  class="font-semibold mt-6">Schlussbestimmungen</Heading>
+    <P>
         Sollten einzelne Bestimmungen dieses Impressums unwirksam sein oder werden, so bleibt die Wirksamkeit der übrigen Bestimmungen unberührt.
-    </p>
+    </P>
 </div>
