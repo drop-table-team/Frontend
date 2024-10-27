4f041a11267638fc40b973b8057d2c8310bc0400
Max Gerbeth <maxgerbeth03@gmail.com>
2024-10-27 02:42:05 +0100

    darkmode

--- src/lib/components/+Navbar.svelte
+++ src/lib/components/+Navbar.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode } from 'flowbite-svelte';
 
     let links = [
         { name: 'Wissen finden', href: '/find' },
@@ -7,15 +7,20 @@
         { name: 'Impressum', href: '/impressum' }
     ];
 </script>
+
 <Navbar>
     <NavBrand href="/">
       <img src="Logo.png" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
       <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Knowledge Base</span>
     </NavBrand>
-    <NavHamburger  />
-    <NavUl >
+    <div class="flex">
+      <NavUl>
       {#each links as link}
         <NavLi href={link.href}>{link.name}</NavLi>
       {/each}
     </NavUl>
+    <NavHamburger  />
+    <DarkMode class="self-right" />
+    </div>
+    
 </Navbar>
